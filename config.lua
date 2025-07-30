932eca4042c2cc7c0e875e1af678aaa2549feb0f
Johan Moszkowicz <advocaatjohand@gmail.com>
2025-07-31 00:50:43 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+-- Notificatie duur in ms
+Config.NotifyTime = 5000
+
+-- Debug mode aan/uit
+Config.Debug = false
+
+-- Max afstand (in meters) dat je mag wegvaren bij anker aan
+Config.MaxAnchorDistance = 5.0
+
+-- Labels voor anker bediening
+Config.Labels = {
+    anchorToggle = "⚓  Anker bedienen",
+}
